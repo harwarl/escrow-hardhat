4f9f7b0b9048d91de7234b63600265f455067636
harwarl <harwarl87@gmail.com>
2024-08-11 23:24:47 +0100

    deploy on sepolia

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,8 +1,17 @@
-require('@nomicfoundation/hardhat-toolbox');
+require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 module.exports = {
   solidity: "0.8.17",
+  // defaultNetwork: "localhost",
   paths: {
     artifacts: "./app/src/artifacts",
-  }
+  },
+  networks: {
+    sepolia: {
+      chainId: 11155111,
+      url: process.env.TEST_RPC_URL,
+      accounts: [process.env.TEST_PRIVATE_KEY],
+    },
+  },
 };
