a22903ab3dc0b29ee603b4ab61ee7000c1091396
harwarl <harwarl87@gmail.com>
2024-08-11 23:24:14 +0100

    deploy on sepolia

--- app/src/index.js
+++ app/src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 if (!window.ethereum) {
+  //window.ethereum represent stuff like metamask
   root.render(
     <React.StrictMode>
       You need to install a browser wallet to build the escrow dapp
